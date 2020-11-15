c499218d5b6aa623d4e1fb79c5ee2a9573cc0ab7
Shaun Kulesa <69469909+ShaunKulesa@users.noreply.github.com>
2020-11-15 15:11:59 +0000

    Update main.py

--- Add properties to all selected objects/main.py
+++ Add properties to all selected objects/main.py
@@ -2,4 +2,6 @@ import bpy
 
 for ob in bpy.context.selected_objects:
     bpy.context.view_layer.objects.active = ob
-    bpy.ops.object.game_property_new()
+    bpy.ops.object.game_property_new(name="Test", type="BOOL")
+    
+#type: 'BOOL', 'INT', 'FLOAT', 'STRING', 'TIMER'
